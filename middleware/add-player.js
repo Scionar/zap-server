e3dd0f4a6472bdfc61f449ead49a14b444822f8a
Joona Viertola <contact@joonaviertola.com>
2017-08-31 00:28:13 +0300

    Prevent adding user with same name.

--- middleware/add-player.js
+++ middleware/add-player.js
@@ -8,18 +8,29 @@ module.exports = function (name, cb) {
   Game.getStatus()
   .then((status) => {
     if (status === Game.GAME_STATUS_OFF) {
-      // Check that there is still space for new player.
       Player.getAll()
       .then((players) => {
-        if (players.length < 3) {
+        let status = 'ok';
+        let message = '';
+
+        // Check that username is not taken.
+        players.forEach((item) => {
+          if (item.name === name) {status = 'error'; message = 'Username already in use.';}
+        });
+
+        // Check that there is still space for new player.
+        if (players.length >= 3)  {status = 'error'; message = 'No more slots for new players.';}
+
+        // If status ok, add user and start game if room full.
+        if (status === 'ok') {
           Player.add(name)
           .then(() => {
             webSocket.get().emit('update playerlist');
             if (players.length === 2) startGame();
-            cb('ok', '');
+            cb(status, message);
           });
         } else {
-          cb('error', 'No more slots for more players.');
+          cb(status, message);
         }
       });
     } else {
